c46fa910456554d5ecb9b12a232e22390004212e
Mark <mark.aaron.fisher@gmail.com>
2017-03-24 12:11:44 -0700

    add routes to teams and teams/new and add skeletal html

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,32 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/teams/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/team-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/all-teams.vtl");
+      String name = request.queryParams("team-name");
+      String description = request.queryParams("team-description");
+      String url = request.queryParams("team-url");
+      int teamSizeLimit = Integer.parseInt(request.queryParams("team-size"));
+      Team newTeam = new Team(name, description, url, teamSizeLimit);
+      // System.out.println(Team.allTeams());
+      model.put("teams", Team.getAllTeams());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/all-teams.vtl");
+      // System.out.println(Team.allTeams());
+      // model.put("teams", Team.allTeams());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
