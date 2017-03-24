8f561c5029dd0ff4857052c6e33a036bc89c988a
Mark <mark.aaron.fisher@gmail.com>
2017-03-24 09:24:43 -0700

    add member attributes and constructor with constructor test

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,3 +1,22 @@
-public class Member {
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Member {
+  private String mName;
+  private String mURL;
+  private String mWork;
+  private String mLanguages;
+  private String mLocation;
+  private static Map<String, Member> allMembers = new HashMap<String, Member>();
+
+  public Member(String name, String url, String work, String languages, String location){
+    mName = name;
+    mURL = url;
+    mWork = work;
+    mLanguages = languages;
+    mLocation = location;
+    allMembers.put(mName, this);
+  }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -1,6 +1,14 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MemberTest {
-
+  @Test
+public void member_instantiatesCorrectly(){
+    Member testMember = new Member("Janice", "http://www.janiceIsAwesome.com", "Garage scientist", "python, javaScript, java", "Portland");
+    assertEquals(true, testMember instanceof Member);
+}
 }
